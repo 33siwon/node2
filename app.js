3d4fbff0285c754d74f73360113562512cc53995
siwon <siwon3376@gmail.com>
2021-03-30 20:18:39 +0900

    'middlewareReview'

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const path = require('path')
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+
 const app = express()
 
 app.set('port', process.env.PORT || 3000)
+
+app.use(morgan('dev'))
+app.use(cookieParser('siwonpassword'))
+app.use(session())
+app.use(express.json())
+app.use(express.urlencoded({ extended : true}))
+
+
 
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname,'index.html'))
 })
 
 app.listen(app.get('port'), ()=>{
-    console.log('3000번 포트에서 대기중 ')
+    console.log('익스프레스 서버 실행')
 })
