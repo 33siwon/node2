9ebc8ade9aece5e90a6d90c03510d7ccc329d729
siwon <siwon3376@gmail.com>
2021-04-01 23:47:53 +0900

    '.'

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
+const dotenv = require('dotenv')
+dotenv.config()
 const express = require('express')
 const path = require('path')
 const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
 const session = require('express-session')
-
+const userRouter = require('./routes/user')
 const app = express()
 
 app.set('port', process.env.PORT || 3000)
 
 app.use(morgan('dev'))
-app.use(cookieParser('siwonpassword'))
-app.use(session())
+app.use(cookieParser('process.env.COOKIE_SECRET'))
+app.use(session(
+    {resave : false,
+    saveUninitialized : false,
+    secret : process.env.COOKIE_SECRET,
+    cookie:{
+    httpOnly : true,
+}}
+))
 app.use(express.json())
 app.use(express.urlencoded({ extended : true}))
-
 
 
 app.get('/', (req,res,next)=>{
@@ -23,8 +31,11 @@ app.get('/', (req,res,next)=>{
         httpOnly: true,
         path:'/'
     })
-    res.sendFile(path.join(__dirname,'index.html'))
+    res.sendFile(path.join(__dirname,'multipart.html'))
 })
+
+app.use('/user', userRouter)
+
 
 app.listen(app.get('port'), ()=>{
     console.log('익스프레스 서버 실행')
